41bb2b2ae3e6cb33d4de9f9351f5486e7baabdf2
Ajay <ajayaurora@gmail.com>
2018-05-22 19:27:18 -0400

    commit
    Removed donate.js file.  JS was added to donate.html

--- NYCDA Grp Project/donate.js
+++
@@ -1,9 +0,0 @@
-$('.submitBtn').click(function(){
-  var cardHolder = $('#cardholder-name').val();
-
-  if(!cardHolder){
-    alert('Please enter your credit card information.');
-  } else{
-    alert('Thank you ' + cardHolder + ' for your donation.');
-  }
-});
