f57d9261fd09d380cdec923d26241540762e0e72
Ajay <ajayaurora@gmail.com>
2018-05-24 19:39:31 -0400

    commit

--- Final_Project/finalPrj.js
+++ Final_Project/finalPrj.js
@@ -1,14 +1,16 @@
-$('.storyBtn').click(function(){
-  var x = $('#story');
-
-  if (x.style.display === "none") {
-      x.style.display = "block";
-  } else {
-      x.style.display = "none";
-  }
-});
-
-
 // $('.storyBtn').click(function(){
-//   $("#story").fadeIn();
+//   var x = $('#story');
+//
+//   if (x.style.display === "none") {
+//       x.style.display = "block";
+//   } else {
+//       x.style.display = "none";
+//   }
 // });
+
+
+$('.storyBtn').click(function(){
+  $("#story").toggle("slow", function(){
+    
+  });
+});
