0eb7b307882f8b68008013b7d410034c9a3f595a
Ajay <ajayaurora@gmail.com>
2018-05-24 19:26:04 -0400

    commit
    added js file

---
+++ Final_Project/finalPrj.js
@@ -0,0 +1,14 @@
+$('.storyBtn').click(function(){
+  var x = $('#story');
+
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+});
+
+
+// $('.storyBtn').click(function(){
+//   $("#story").fadeIn();
+// });
