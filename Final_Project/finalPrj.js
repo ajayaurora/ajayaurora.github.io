3fee6aeec339ac96987a62e734d0668f7daa29e8
Ajay <ajayaurora@gmail.com>
2018-05-30 15:53:03 -0400

    commit

--- Final_Project/finalPrj.js
+++ Final_Project/finalPrj.js
@@ -1,5 +1,44 @@
 $('.storyBtn').click(function(e){
   e.preventDefault();
   $("#story").fadeToggle();
-  // $("#story").fadeIn("slow");
+  $("#photo").hide();
 });
+$('.photoBtn').click(function(e){
+  e.preventDefault();
+  $("#photo").fadeToggle();
+  $("#story").hide();
+});
+
+// $('.storyBtn').on('click',
+//   function(e){
+//   e.preventDefault();
+//   $("#story").fadeToggle();
+// });
+
+// $('.photoBtn').on('click',
+//   function(e){
+//   e.preventDefault();
+//   $("#photo").fadeToggle();
+// });
+
+// $('#story').hide();
+// $('.photoBtn').on('click',
+//   function(e) {
+//     e.preventDefault();
+//     $('#story, #photo').toggle(200);
+//   }
+// );
+
+
+// $("#story").hide();
+// $("#photo").hide();
+//
+//
+// $(".storyBtn").click(function(){
+//     $("#story").toggle();
+//     $("#photo").hide();
+// });
+// $(".photoBtn").click(function(){
+//     $("#photo").toggle();
+//     $("#story").hide();
+// });
